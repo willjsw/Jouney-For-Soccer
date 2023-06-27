cc89058f77d06942aa7f46a9187f655cb5c50101
정선우 <sunwoo@SunwooAir.local>
2023-06-27 18:30:13 +0900

    exe file added, http error handling in image url processing logic revised

--- airportDataCrawler.py
+++ airportDataCrawler.py
@@ -6,17 +6,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import chromedriver_autoinstaller
 
+#경기장 최근거리 공항 크롤링 함수
 def crawlNearestAirport(stadium):  
 
+    #크롤링 성능 개선 위한 크롬드라이버 옵션 세팅
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
     options.add_argument("--disable-gpu")
     caps = DesiredCapabilities().CHROME
     caps["pageLoadStrategy"] = "none"
 
+    #크롬드라이버 버전 업데이트 자동화
     chromedriver_autoinstaller.install()  
     
     driver = webdriver.Chrome(options=options)
+    #웹페이지 요소 로딩을 위한 대기 시간 설정
     wait = WebDriverWait(driver, 30)
     
     try:
@@ -27,6 +31,7 @@ def crawlNearestAirport(stadium):
         searchBox.send_keys(Keys.ENTER)
         airportKor = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[1]/h1')))
         airportLocal = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/h2/span')))
+        #결과-> 리스트 형태로 리턴
         return [airportLocal.text,airportKor.text]
     except Exception as e:
         print(e)
--- matchDataCrawler.py
+++ matchDataCrawler.py
@@ -4,33 +4,39 @@ from urllib.error import HTTPError
 import json
 import base64
 
-
+#크롤링 결과 중 일부 커스터마이징
 def fullFormTeam(team):
     if team == "맨유":
         return "맨체스터 유나이티드"
     else:
         return team
-    
+#크롤링한 팀 별 엠블럼 url -> base64 포맷으로 변환해서 열기(http에러 발생 시 로컬에 저장된 기본 이미지 파일로 대체)
 def openImageUrl(imgUrl):
     try:
         img = urlopen(imgUrl).read()
         return base64.encodebytes(img) 
     except HTTPError as e:
         code = e.getcode()
-        print(code+": impropal image url")
+        print(str(code)+": impropal image url")
         return base64.b64encode(open("defaultEmblem.png", "rb").read())
 
-        
+#main 코드에서 빈번하게 사용되는 크롤링한 데이터를 객체로 관리하기 위해 class 선언
+#크롤링한 xhr 파일 본문의json 데이터 문자열화 및 파싱      
 class MatchData:
+
+    #클래스 내 self 변수들 초기화
     def __init__(self):
         self.monthlyScheduleDailyGroup = []
         self.dateTuple = ()
-     
+    
+    #크롤링 및 json 파싱
     def crawlMatchData(self,year,month,league):
         try:
             url = "https://sports.news.naver.com/wfootball/schedule/monthly?year="+year+"&month="+month+"&category="+league
             html = urlopen(url)
+            #bs4 파싱 성능 개선 위해 lxml 모듈 사용
             soup = BeautifulSoup(html, "lxml")
+            #xhr 파일 본문의 json 데이터 역직렬화
             data = json.loads(soup.find("p").getText())
             self.monthlyScheduleDailyGroup = data["monthlyScheduleDailyGroup"]
             html.close()
@@ -38,43 +44,45 @@ class MatchData:
         except Exception as e:
             self.monthlyScheduleDailyGroup = None
             print(e)
-        
+    #크롤링한 조건(리그, 연월, 월)에 해당되는 경기 일자 튜플로 구성
     def getMatchDates(self):
         dateList = []
         for matchOfDate in self.monthlyScheduleDailyGroup:
             dateList.append(matchOfDate["date"])
         self.dateTuple = tuple(dateList)
         return self.dateTuple
-
+    
+    #입력된 날짜에 해당하는 경기 정보 딕셔너리 형태로 반환
     def getMatches(self,date):
         index = self.dateTuple.index(date)
         matchOfDate = self.monthlyScheduleDailyGroup[index]
         return matchOfDate["scheduleList"]
-
+    
+    #홈 팀 이름 가져오기
     def homeTeamName(self, match):
        return fullFormTeam(match["homeTeamName"])
-    
+    #어웨이 팀 이름 가져오기
     def awayTeamName(self, match):
        return fullFormTeam(match["awayTeamName"])
-    
+    #홈 팀 점수 가져오기
     def homeTeamScore(self, match):
        return match["homeTeamScore"]
-    
+    #어웨이 팀 점수 가져오기
     def awayTeamScore(self, match):
        return match["awayTeamScore"]
-
+    #홈 팀 엠블럼 가져오럼
     def homeTeamEmblem(self, match):
         return openImageUrl(match["homeTeamEmblem64URI"])
-
+    #홈 팀 엠블럼 가져오기
     def awayTeamEmblem(self, match):
        return openImageUrl(match["awayTeamEmblem64URI"])
-    
+    #경기 시작 시간 가져오기
     def gameStartTime(self, match):
        return match["gameStartTime"]
-    
+    #경기장 정보 가져오기
     def stadium(self, match):
        return match["stadium"]
-    
+    #경기 승자 가져오기
     def winner(self,match):
         if match["homeTeamWon"]==True and match["awayTeamWon"]==False:
             return "home"
